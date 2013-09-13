f0c4262c08af5b4cc6dc837c4ee274e311c1346e
krisdestruction <krisdestruction@gmail.com>
2013-09-12 23:07:50 -0400

    Added scan inventory functionality

--- src/info/terrismc/TekkitCustomizer/PlayerScanner.java
+++ src/info/terrismc/TekkitCustomizer/PlayerScanner.java
@@ -1,8 +1,15 @@
 package info.terrismc.TekkitCustomizer;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+
 public class PlayerScanner {
 
 	public PlayerScanner( TekkitCustomizer plugin ) {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void scanInventory( Player player ) {
+		PlayerInventory inventory = player.getInventory();
 	}
 }
--- src/info/terrismc/TekkitCustomizer/WorldScanner.java
+++ src/info/terrismc/TekkitCustomizer/WorldScanner.java
@@ -1,9 +1,11 @@
 package info.terrismc.TekkitCustomizer;
 
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.PlayerInventory;
+
 public class WorldScanner {
 
 	public WorldScanner( TekkitCustomizer plugin ) {
 		// TODO Auto-generated constructor stub
 	}
-
 }
