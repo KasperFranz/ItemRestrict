b6ba4f8db168ed90b1097c38679a3873c37d64bf
krisdestruction <krisdestruction@gmail.com>
2013-09-13 00:17:56 -0400

    Added scanner functions

--- src/info/terrismc/TekkitCustomizer/PlayerScanner.java
+++ src/info/terrismc/TekkitCustomizer/PlayerScanner.java
@@ -1,15 +1,21 @@
 package info.terrismc.TekkitCustomizer;
 
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
 public class PlayerScanner {
-
+	private ConfigStore cStore;
+	
 	public PlayerScanner( TekkitCustomizer plugin ) {
-		// TODO Auto-generated constructor stub
+		this.cStore = plugin.cStore;
 	}
 	
 	public void scanInventory( Player player ) {
 		PlayerInventory inventory = player.getInventory();
+		ItemStack[] items = inventory.getContents();
+		for( int i = 0; i < items.length; i++ )
+			if( cStore.isBanned( items[i] , ActionType.Ownership ) )
+				inventory.setItem( i, null );
 	}
 }
