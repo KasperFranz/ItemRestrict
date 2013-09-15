e2aec29d69377fbd15a573b1eec4f82555293e95
Chris Woo <krisdestruction@gmail.com>
2013-09-14 22:43:08 -0400

    Add scan chunk functionality

--- src/info/terrismc/TekkitCustomizer/QuickStore.java
+++ src/info/terrismc/TekkitCustomizer/QuickStore.java
@@ -1,17 +1,16 @@
 package info.terrismc.TekkitCustomizer;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.bukkit.Chunk;
-import org.bukkit.ChunkSnapshot;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.scheduler.BukkitScheduler;
 
 public class QuickStore {
-	private TekkitCustomizer plugin;
+	final private TekkitCustomizer plugin;
 	private ConfigStore cStore;
 	
 	public QuickStore( TekkitCustomizer plugin ) {
@@ -20,7 +19,32 @@ public class QuickStore {
 	}
 	
 	public void scanChunk( final Chunk chunk ) {
-		
+		final BukkitScheduler scheduler = TekkitCustomizer.server.getScheduler();
+		scheduler.runTaskAsynchronously( plugin, new Runnable() {
+			public void run() {
+				final int yMax = chunk.getWorld().getMaxHeight();
+				Block block;
+				for( int x = 0; x < 32; x++ ) {
+					for( int z = 0; z < 32; z++ ) {
+						for( int y = 0; y < yMax; y++ ) {
+							block = chunk.getBlock( x, y, z );
+							if( cStore.isBannable( block, ActionType.Ownership ) ) {
+								block.setType( Material.AIR );
+								/*
+								// Schedule synchronous set
+								final Block runnableBlock = block;
+								scheduler.runTask( plugin, new Runnable() {
+									public void run() {
+										runnableBlock.setType( Material.AIR );
+									}
+								});
+								*/
+							}
+						}
+					}
+				}
+			}
+		});
 	}
 	
 	public void scanInventory( final Player player ) {
