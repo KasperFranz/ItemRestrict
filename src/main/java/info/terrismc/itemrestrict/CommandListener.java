dd71417a78d163d3bd4a27057de623e75c2ecf5f
kasper Franz <kasper@franz.guru>
2016-03-27 16:04:17 +0200

    Format

--- src/main/java/info/terrismc/itemrestrict/CommandListener.java
+++ src/main/java/info/terrismc/itemrestrict/CommandListener.java
@@ -5,50 +5,52 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CommandListener implements CommandExecutor {
-	private ConfigStore cStore;
-	
-	public CommandListener( ItemRestrict plugin ) {
-		this.cStore = plugin.cStore;
-	}
 
-	public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
-		if( args.length == 0 )
-			return false;
-		switch( args[0] ) {
-		case "reload":
-			if( args.length == 1 ) {
-				cStore.reloadConfig();
-				sender.sendMessage( "ItemRestrict Config Reloaded" );
-				return true;	
-			}
-		case "ban":
-			if( args.length == 3 ) {
-				cStore.addBan( sender, getActionType( args[1] ), args[2] );
-				return true;
-			}
-		case "unban":
-			if( args.length == 3 ) {
-				cStore.removeBan( sender, getActionType( args[1] ), args[2] );
-				return true;
-			}
-		}
-		return false;
-	}
-	
-	private ActionType getActionType( String actionTypeString ) {
-		// Select proper string
-		switch( actionTypeString.toLowerCase() ) {
-		case "usage":
-			return ActionType.Usage;
-		case "equip":
-			return ActionType.Equip;
-		case "crafting":
-			return ActionType.Crafting;
-		case "ownership":
-			return ActionType.Ownership;
-		case "world":
-			return ActionType.World;
-		}
-		return null;
-	}
+    private ConfigStore cStore;
+
+    public CommandListener(ItemRestrict plugin) {
+        this.cStore = plugin.cStore;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String alias, String[] args) {
+        if (args.length == 0) {
+            return false;
+        }
+        switch (args[0]) {
+            case "reload":
+                if (args.length == 1) {
+                    cStore.reloadConfig();
+                    sender.sendMessage("ItemRestrict Config Reloaded");
+                    return true;
+                }
+            case "ban":
+                if (args.length == 3) {
+                    cStore.addBan(sender, getActionType(args[1]), args[2]);
+                    return true;
+                }
+            case "unban":
+                if (args.length == 3) {
+                    cStore.removeBan(sender, getActionType(args[1]), args[2]);
+                    return true;
+                }
+        }
+        return false;
+    }
+
+    private ActionType getActionType(String actionTypeString) {
+        // Select proper string
+        switch (actionTypeString.toLowerCase()) {
+            case "usage":
+                return ActionType.Usage;
+            case "equip":
+                return ActionType.Equip;
+            case "crafting":
+                return ActionType.Crafting;
+            case "ownership":
+                return ActionType.Ownership;
+            case "world":
+                return ActionType.World;
+        }
+        return null;
+    }
 }
